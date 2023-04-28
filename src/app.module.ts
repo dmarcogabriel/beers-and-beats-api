9e1f2bb6729f67b4d768400c1597c841d878d5a4
Gabriel De Marco <lino.dmarco@gmail.com>
2023-04-28 14:41:05 -0300

    feat: add database connection config

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const dbPassword = configService.get<string>('DATABASE_PASSWORD');
+        const dbName = configService.get<string>('DATABASE_NAME');
+
+        if (dbPassword && dbName) {
+          const password = encodeURIComponent(dbPassword);
+          return {
+            uri: `mongodb+srv://gabriel:${password}@cluster0.bkb6b.mongodb.net/${dbName}?retryWrites=true&w=majority`,
+          };
+        }
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
