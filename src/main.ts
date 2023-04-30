6a8fb7ef465e5329311cb655df48cfff9ed40ef1
Gabriel De Marco <lino.dmarco@gmail.com>
2023-04-30 12:22:27 -0300

    feat: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Beers and Beats example')
+    .setDescription(
+      "This API contains a CRUD for beers and a find by temperature endpoint, it doesn't need authentication to use it.",
+    )
+    .setVersion('1.0')
+    .addTag('beers')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
