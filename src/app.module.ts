8a852fc78f545fef8518fe977525bb3823aaa4a5
Gabriel De Marco <lino.dmarco@gmail.com>
2023-04-29 21:20:09 -0300

    feat: remove unused app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BeersModule } from './beers/beers.module';
 
 @Module({
@@ -26,6 +25,5 @@ import { BeersModule } from './beers/beers.module';
     BeersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
